f88767e0a584bc0f5f5d0f9d4f390c04e59b999e
AparnaTerzo <aparna@terzocloud.com>
2023-06-12 15:09:49 +0530

    Initial version

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            if(i%2 ==0){
+                System.out.println("Even Number :" + i);
+            }
+            else{
+                System.out.println("Odd Number :" + i);
+            }
+        }
+    }
+}
